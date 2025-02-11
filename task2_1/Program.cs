f50296b30bff0706887816d4bd954a8cf1f64b16
dezag <98271110+dsiryk@users.noreply.github.com>
2025-02-11 21:46:59 +0200

    Add files via upload

---
+++ task2_1/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введiть будь яке число для перевiрки, (діапазон від 10 до 50):");
+double a = Convert.ToDouble(Console.ReadLine());
+if (a >= 10 && a <= 50)
+    Console.WriteLine(true);
+else Console.WriteLine(false);
