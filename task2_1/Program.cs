9b104e6bd62f7a27641eef1cb82108baed85bb5c
dezag <98271110+dsiryk@users.noreply.github.com>
2025-02-13 16:17:34 +0200

    Update Program.cs

--- task2_1/Program.cs
+++ task2_1/Program.cs
@@ -1,6 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Введiть будь яке число для перевiрки, (дiапазон вiд 10 до 50):");
+﻿Console.WriteLine("Введiть будь яке число для перевiрки, (дiапазон вiд 10 до 50):");
 double a = Convert.ToDouble(Console.ReadLine());
-if (a >= 10 && a <= 50)
-    Console.WriteLine(true);
-else Console.WriteLine(false);
+string res = a >= 10 && a <= 50 ? "Число в дiапазонi" : "Не в дiапазонi";
+Console.WriteLine(res);
